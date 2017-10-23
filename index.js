3f01e38399228f15ccca045195558f59be118611
Bruno Dmello <brunodmello@Yapbooks-MacBook-Pro.local>
2017-10-23 16:40:52 +0530

    second prototype

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Hapi = require('hapi');
 const fs = require('fs');
 const path = require('path');
 _   = require('lodash');
+const toPdf = require("office-to-pdf")
 
 let dictionary = require('./dictionary.json');
 const dust = require('dustjs-linkedin');
@@ -32,16 +33,10 @@ server.register(require('vision'), (err) => {
     })
 });
 
-// fs.readFile(path.join(__dirname, 'templates/index.html'), { encoding: 'utf8' }, function(err, data) {
-//   dust.loadSource(data);
-//   dust.render(templateName, {}, function(err, out) {
-//     console.log(out);
-//   });
-// });
 
 //console.log("dictionary", dictionary);
 // Add the route
-server.route({
+server.route([{
     method: 'GET',
     path:'/', 
     handler: function (request, reply) {
@@ -51,7 +46,30 @@ server.route({
         });
         
     }
-});
+}, {
+    method: 'GET',
+    path:'/list/{encname}', 
+    handler: function (request, reply) {
+            //reply.view('index');request.params.encname
+            let encname = request.params.encname;
+            if(!encname){
+                return reply("<h3>File name is incorrect</h2>");
+
+            }
+
+            let fileName  = new Buffer(encname, 'base64').toString()
+
+            //console.log("getPPTPath(fileName)", getPPTPath(fileName));
+
+            fs.stat(getPPTPath(fileName), function(err, stat) {
+                if(err == null) {
+                    
+                } else
+                    return reply("<h3>File doesnot exist</h3>")
+            });
+        
+    }
+}]);
 
 createDictionary(function(){
     server.start((err) => {
@@ -59,7 +77,7 @@ createDictionary(function(){
             throw err;
         }
         
-        let preCompiledTemplateSrc = fs.readFileSync(path.join(__dirname, 'templates/index.dust'), 'utf8');
+        let preCompiledTemplateSrc = fs.readFileSync(getTemplatePath('index'), 'utf8');
         //dust.loadSource(list);
         dust.loadSource(dust.compile(preCompiledTemplateSrc, 'dictionary'));
         console.log('Server running at:', server.info.uri);
@@ -67,37 +85,54 @@ createDictionary(function(){
 })
 // Start the server
 
+function getTemplatePath(name){
+    return path.join(__dirname, `templates/${name}.dust`)
+}
+function getPPTPath(name){
+    return path.join(__dirname, `ppts/${name}`);
+}
+
 function createDictionary(onComplete) {
     fs.readdir(path.resolve(__dirname, 'ppts'), function(err, filenames) {
-            //let dictionary = {};
-            _.each(dictionary, function(datum){
-                let alpha = _.lowerCase(datum.group);
-                console.log("alpha", alpha);
-                let foundName = _.find(filenames, function(fileName){
-                    if(_.startsWith(fileName, (alpha)))
-                        return fileName;
-                });
-                if(foundName)
-                    datum.list.push(foundName);
 
-            })
+        _.each(filenames, function(filename){
+            let encfilename = new Buffer(filename).toString('base64');
+            var wordBuffer = fs.readFileSync(path.resolve(__dirname, `ppts/${filename}`))
+            // toPdf(wordBuffer)
+            // .then(
+            //   (pdfBuffer) => {
+            //     fs.writeFileSync(path.resolve(__dirname, `ppts/${encfilename}.pdf`), pdfBuffer)
+            //   }, (err) => {
+            //     console.log(err)
+            //   }
+            // )
+            // .then(()=>{
+
+            //     //Creating dictionary
+            //     _.each(dictionary, function(datum){
+            //         let alpha = _.lowerCase(datum.group);
+            //         //console.log("alpha", alpha);
+            //         let foundName = _.startsWith(filename, (alpha)) ? true : false;
+            //         if(foundName)
+            //             datum.list.push({
+            //                 name : foundName,
+            //                 enc : encfilename
+            //             });
+
+            //     })
+            // })    
+        })
+
+
+
+
+
+            
+            
             //console.log("dictionary", JSON.stringify(dictionary));
             onComplete();
             //reply(filenames);
     });
-  //fs.readdir(path.resolve(__dirname, './ppts'), function(err, filenames) {
-    // if (err) {
-    //   onError(err);
-    //   return;
-    // }
-    // filenames.forEach(function(filename) {
-    //   fs.readFile(path.resolve(dirname, filename), 'utf-8', function(err, content) {
-    //     if (err) {
-    //       onError(err);
-    //       return;
-    //     }
-    //     onFileContent(filename, content);
-    //   });
-    // });
-  //});
 }
+
+
